ca8774cd4c236f5d8232b5ab89fad2f2e73e18a4
Jeff Dyke <jeff.dyke@gmail.com>
2025-02-16 12:13:14 -0500

    update ci to be functions, call from main

---
+++ ecr-auth-v2/build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+source ci/build.sh
+
+clean
+build
--- ecr-auth-v2/ci/build.sh
+++ ecr-auth-v2/ci/build.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-docker build --build-arg REGION=us-east-1 -t ecr-auth-v2 ecr-auth-v2
-docker tag ecr-auth-v2:latest jeffdyke/ecr-auth-v2:latest
-docker push jeffdyke/ecr-auth-v2
+clean() {
+  docker images | grep ecr-auth | awk '{print $3}' | sort | uniq | xargs sudo docker rmi --force
+  sudo docker builder prune -f
+}
+build() {
+  docker build --build-arg REGION=us-east-1 -t ecr-auth-v2 ecr-auth-v2
+  docker tag ecr-auth-v2:latest jeffdyke/ecr-auth-v2:latest
+}
+push() {
+  docker push jeffdyke/ecr-auth-v2
+}
